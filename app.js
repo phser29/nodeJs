8dd7c3bb214e56e205cb7ed99bbe0adbe0a963bc
phs28 <104198099+phser29@users.noreply.github.com>
2024-08-15 19:11:30 +0900

    commit

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const port = 3000
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
 //라우팅
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.render('index'); // ./views/index.ejs
 })
 
-app.get('/test', (req, res) => {
-  res.send('test!')
+app.get('/profile', (req, res) => {
+  res.render('profile'); // ./views/index.ejs
 })
+
 
 app.listen(port, () => {
   console.log(`서버가 실행되었습니다. ${port}`)
